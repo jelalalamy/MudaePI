dfb7e133ad7290694b72035b50f911461fec5b55
Jeremy La <jeremylai511@gmail.com>
2022-09-04 23:01:46 -0400

    Added hover effect to Tile component

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import placeholder from '../placeholder.png';
 
 const Tile = ({ char }) => {
   return (
-    <div className="tile">
-        <p>Name: {char.name}</p>
-        <p>Series: {char.series}</p>
-        <p>Rank: {char.rank}</p>
-        <p>Kakera: {char.kakera}</p>
+    <div className="tileWrap">
+      <img className="tileImage" src={placeholder}/>
+      <div className="tileTextLayer">
+        <div className="tileText">
+            <p>Name: {char.name}</p>
+            <p>Series: {char.series}</p>
+            <p>Rank: {char.rank}</p>
+            <p>Kakera: {char.kakera}</p>
+        </div>
+      </div>
     </div>
   )
 }
