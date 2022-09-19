60c52225fa75dbef990e640a3090686bf33f8338
Jeremy La <jeremylai511@gmail.com>
2022-09-19 00:00:00 -0400

    Added Header to all Pages

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Header from './Header'
 
 export const About = () => {
   return (
-    <div className="aboutWrapper">
-      <h1 className="aboutHeader">About</h1>
-      <p className="aboutBody">MudaePI (a play on the words Mudae and API) is an unofficial web app based on the popular Discord bot/game Mudae.
-      Here, you can view the top ranked characters for the day, view snapshots of past days, and compare different snapshots to find the hottest
-      characters at the moment. Where does the API come in? You'll find out soon.</p>
+    <div>
+      <Header/>
+      <div className="aboutWrapper">
+        <h1 className="aboutHeader">About</h1>
+        <p className="aboutBody">MudaePI (a play on the words Mudae and API) is an unofficial web app based on the popular Discord bot/game Mudae.
+        Here, you can view the top ranked characters for the day, view snapshots of past days, and compare different snapshots to find the hottest
+        characters at the moment. Where does the API come in? You'll find out soon.</p>
+      </div>
     </div>
   )
 }
--- src/components/WIP.js
+++ src/components/WIP.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Header from './Header'
 
 const WIP = () => {
   return (
-    <div className="wipWrapper">
-      <h1>Coming soon...</h1>
-    </div>
+    <div>
+      <Header/>
+      <div className="wipWrapper">
+        <h1>Coming soon...</h1>
+      </div>
+      </div>
   )
 }
 
